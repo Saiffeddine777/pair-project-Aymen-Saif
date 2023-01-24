855315138e9850ead5ff6fc1e5dad026cd2312d9
unknown <aymenbougeh1@gmail.com>
2023-01-24 12:59:19 +0100

    finalized

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
+var results =  []
 
 var i=0
 $("#btnadd").click(function(){
-    
-    $("#table").append("<tr><th id='nn"+i+"'>"+$('#newstudent').val()+"</th><th><input type='number' id='p"+i+"'></th><th><input type='number' id='a"+i+"'></th><th><input type='text' id='w"+i+"'</th><th><input type='text' id='c"+i+"'</th><th><input type='text' id='pr"+i+"'</th><th><input type='text' id='as"+i+"'</th><th><input type='text' id='av"+i+"'</th><th><input type='text' id='ev"+i+"'></th><th><button type='button' id='del'>Remove</button></th></tr>")
+    $("#table").append("<tr id='jj"+i+"'><th id='nn"+i+"'>"+$('#newstudent').val()+"</th><th><input type='number' id='p"+i+"'></th><th><input type='number' id='a"+i+"'></th><th><input type='text' id='w"+i+"'</th><th><input type='text' id='c"+i+"'</th><th><input type='text' id='pr"+i+"'</th><th><input type='text' id='as"+i+"'</th><th><input type='text' id='av"+i+"'</th><th><input type='text' id='ev"+i+"'></th><th><input type='button' id='del"+i+"' onclick='del("+i+")' value='remove'></th></tr>")
     $('#newstudent').val("") 
 i++
 })
@@ -16,7 +15,11 @@ $("#bt").click(function(e){
     e.preventDefault()
     for(var k=0;k<i;k++){
     var averg=((($("#w"+k).val()*1)*1.5)+($("#c"+k).val()*1)+(($("#pr"+k).val()*1)*4)+(($("#as"+k).val()*1)*3))/9.5
-    console.log(averg)
+    
+    var obj = {}
+    obj.name = $("#nn"+k).text()
+    obj.average = averg
+    results.push(obj)
     $("#av"+k).val(averg.toFixed(2))
     if(averg>10){
         $("#ev"+k).val("Pass")
@@ -31,21 +34,25 @@ $("#bt").click(function(e){
 
 
 
+$("#best").click(function(results){
+    var temp
+    var max=results[0].average
+    for(var j=0;j<=i;j++){
+        
+    if(results[j].average>=max){
+        max=results[j].average 
+        temp = result[j].name
 
-
-/*$("#del").click(function() {
-    
-    $("#table").deleteRow(1);
     }
-  )*/
 
-/*$("#best").click(function(){
-    var max=$("#av0").val()
-    for(var k=0;k<i;k++){
-    if($("#av"+k).val()>$("#av0").val()){
-        max=$("#av"+k).val()
-    }
 }
-    
-})*/
 
+alert (temp+" is the best with an average of"+max )
+})
+
+function del (i){
+    $("#jj"+i).remove()
+    
+}
+
+console.log(results)
